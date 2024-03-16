7a41c4e8dcbde8fa79d8d04d6951c8c993e55f28
Amit Patel <amitpatel1701@yahoo.com>
2024-03-16 16:40:15 -0400

    Different ways to inject a dependency, spring way anotated

--- src/test/java/guru/springframework/spring6di/controller/ConstructorInjectControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controller/ConstructorInjectControllerTest.java
@@ -3,17 +3,21 @@ package guru.springframework.spring6di.controller;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ConstructorInjectControllerTest {
 
+    @Autowired
     ConstructorInjectController constructor;
 
-    @BeforeEach
-    void setUp() {
-        constructor = new ConstructorInjectController(new GreetingServiceImpl());
-    }
+//    @BeforeEach
+//    void setUp() {
+//        constructor = new ConstructorInjectController(new GreetingServiceImpl());
+//    }
 
     @Test
     void sayHello() {

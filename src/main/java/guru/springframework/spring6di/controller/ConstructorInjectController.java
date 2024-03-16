048d4a8957c12f80be7d40979def3a67d098afe7
Amit Patel <amitpatel1701@yahoo.com>
2024-03-16 17:16:20 -0400

    DI - Spring Qualifiers

--- src/main/java/guru/springframework/spring6di/controller/ConstructorInjectController.java
+++ src/main/java/guru/springframework/spring6di/controller/ConstructorInjectController.java
@@ -1,13 +1,14 @@
 package guru.springframework.spring6di.controller;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectController(GreetingService greetingService) {
+    public ConstructorInjectController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/guru/springframework/spring6di/controller/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controller/SetterInjectedController.java
@@ -2,14 +2,15 @@ package guru.springframework.spring6di.controller;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
 
-
     private GreetingService greetingService;
 
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         System.out.println("SetterInjectedController.setGreetingService");
